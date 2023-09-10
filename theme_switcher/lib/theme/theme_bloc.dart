64b0bf0aaf2274e5f8756b3b3f512e36096c4e85
Sameer Jagtap <jagtap.sameer00@gmail.com>
2023-09-10 20:56:22 +0530

    Made theme change when rand int is odd or even

--- theme_switcher/lib/theme/theme_bloc.dart
+++ theme_switcher/lib/theme/theme_bloc.dart
@@ -1,13 +1,18 @@
 import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 part 'theme_event.dart';
 part 'theme_state.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
-  ThemeBloc() : super(ThemeInitial()) {
-    on<ThemeEvent>((event, emit) {
-      // TODO: implement event handler
+  ThemeBloc() : super(ThemeState.initial()) {
+    on<ChangeThemeEvent>((event, emit) {
+      if (event.randInt % 2 == 0) {
+        emit(state.copyWith(appTheme: Apptheme.light));
+      } else {
+        emit(state.copyWith(appTheme: Apptheme.dark));
+      }
     });
   }
 }
--- theme_switcher/lib/theme/theme_state.dart
+++ theme_switcher/lib/theme/theme_state.dart
@@ -1,6 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'theme_bloc.dart';
 
-@immutable
-sealed class ThemeState {}
+enum Apptheme { light, dark }
 
-final class ThemeInitial extends ThemeState {}
+class ThemeState extends Equatable {
+  final Apptheme appTheme;
+
+  ThemeState({
+    this.appTheme = Apptheme.light,
+  });
+
+  factory ThemeState.initial() {
+    return ThemeState();
+  }
+
+  @override
+  List<Object> get props => [Apptheme];
+
+  ThemeState copyWith({
+    Apptheme? appTheme,
+  }) {
+    return ThemeState(
+      appTheme: appTheme ?? this.appTheme,
+    );
+  }
+
+  @override
+  String toString() => 'ThemeState(appTheme: $appTheme)';
+}
